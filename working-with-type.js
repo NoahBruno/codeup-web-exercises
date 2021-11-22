1436d2e992d4fa4c98b2c368fc3703d7f39b6b9a
Noah Bruno <noah.bruno122@gmail.com>
2021-11-22 15:10:29 -0600

    working-with-type.js

---
+++ working-with-type.js
@@ -0,0 +1,78 @@
+var a = 1;
+console.log(a)
+var b = a++;
+console.log(a)
+var c = ++a;
+console.log(a)
+
+var d = "hello";
+var e = false;
+d++;
+console.log(d)
+e++;
+console.log(e)
+
+var perplexed;
+console.log(perplexed + 2)
+
+var price = 2.7;
+console.log(price.toFixed(2))
+
+var price = "2.7";
+console.log(parseFloat(price).toFixed(2))
+
+console.log(isNaN(0))
+
+console.log(isNaN(1))
+
+console.log(isNaN(""))
+
+console.log(isNaN("string"))
+
+console.log(isNaN("0"))
+
+console.log(isNaN("1"))
+
+console.log(isNaN("3.145"))
+
+console.log(isNaN(Number.MAX_VALUE))
+
+console.log(isNaN(Infinity))
+
+console.log(isNaN("true"))
+
+console.log(isNaN(true))
+
+console.log(isNaN("false"))
+
+console.log(isNaN(false))
+
+console.log(NaN == NaN)
+
+console.log(!true)
+
+console.log(!false)
+
+console.log(!!true)
+
+console.log(!!false)
+
+console.log(!!0)
+
+console.log(!!-0)
+
+console.log(!!1)
+
+console.log(!!-1)
+
+console.log(!!0.1)
+
+console.log(!!"hello")
+
+console.log(!!"")
+
+console.log(!!'')
+
+console.log(!!"false")
+
+console.log(!!"0")
