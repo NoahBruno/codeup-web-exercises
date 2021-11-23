1377b83d6501cd0534f4fa61bfd5cc21d49eb9fd
Noah Bruno <noah.bruno122@gmail.com>
2021-11-22 21:38:12 -0600

    Primitive-type.js

--- Primitive type.js
+++ Primitive type.js
@@ -1,7 +1,14 @@
 console.log(typeof 99);
+//number
 console.log("false");
+//false
 console.log(false);
+//false
 console.log('0');
+//0
 console.log(0);
+//0
 console.log('true');
+//true
 console.log(true);
+//true
