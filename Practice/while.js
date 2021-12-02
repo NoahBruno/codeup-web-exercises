58b7260e2b37a70df175b369dae4d6e7b22a3f29
Noah Bruno <noah.bruno122@gmail.com>
2021-12-02 15:38:59 -0600

    loops

--- Practice/while.js
+++ Practice/while.js
@@ -4,3 +4,18 @@ while (i < 66536){
     i*=2;
 
 }
+
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var ran= Math.floor(Math.random() * 5) + 1;
+
+
+
+
+do {
+    console.log("less " + allCones);
+    allCones-=ran;
+}while ( allCones>=0);
+console.log("cones all gone")
