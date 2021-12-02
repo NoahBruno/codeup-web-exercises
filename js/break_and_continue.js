82a6fdc8e2d68a57a9dee1df96c083a7d4f2f8a2
Noah Bruno <noah.bruno122@gmail.com>
2021-12-01 20:49:46 -0600

    practice

---
+++ js/break_and_continue.js
@@ -0,0 +1,9 @@
+
+var oddNumber = prompt("type in an odd number between 1 and 50");
+var answer = oddNumber
+
+do {
+    alert ("That's not an odd number!")
+}while (answer % 2 !== 0){
+    continue;
+}
