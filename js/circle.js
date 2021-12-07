463fe4914049a361912e83635d89f0dec4d99a5b
Noah Bruno <noah.bruno122@gmail.com>
2021-12-07 14:33:55 -0600

    math

--- js/circle.js
+++ js/circle.js
@@ -3,22 +3,22 @@
 
     // create a circle object
     var circle = {
-        radius: 3,
+        radius: 5,
 
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return Math.PI * Math.pow(this.radius,2); // TODO: return the proper value
+            return Math.PI * (this.radius ** 2); // TODO: return the proper value
         },
 
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (this.getArea() % 1 !=0){
-                return false;
+                return Math.round(this.getArea());
             }else{
-                return true;
+                return this.getArea();
             }
 
             // If doRounding is true, round the result to the nearest integer.
@@ -27,6 +27,8 @@
             console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
     };
+
+    console.log("Area", circle.getArea());
 
     // log info about the circle
     console.log("Raw circle information");
