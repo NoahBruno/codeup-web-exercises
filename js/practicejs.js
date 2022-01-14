660102a893a46431e2e1d0943b5f264d19e321af
Noah Bruno <noah.bruno122@gmail.com>
2022-01-14 10:29:15 -0600

    coffee

---
+++ js/practicejs.js
@@ -0,0 +1,8 @@
+// Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"] and returns a new array that contains only those whose name have only 4 letters in them.
+
+var names = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+
+
+for (i = 0, i < names.length; i++; ){
+
+}
