11cd1fb5a8bf19fd3c8d207d76b816e2d9adc2cc
Noah Bruno <noah.bruno122@gmail.com>
2022-01-17 14:36:35 -0600

    wow

--- js/practicejs.js
+++ js/practicejs.js
@@ -2,10 +2,31 @@
 
 var names = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
 
+// for (var i = 0; i < names.length; i++){
+//     if (newName > 4){
+//         console.log(names)
+//     }
+// }
 
-for (var i = 0, i < names.length; i++){
-    if (i > 4){
-        console.log(names[i])
+// names.forEach(function (name){
+//     if (parseInt(name) > 4){
+//         console.log("more than 4 " + name)
+//     }else{
+//         console.log("nope")
+//     }
+// })
+
+// let name = names.join(" ")
+// console.log(name)
+
+for (var i = 0; i < names.length; i++){
+    if(names[i].length > 4){
+        console.log("The names " + names[i] + ", James " + names[i])
+    }else{
+        console.log("nope")
     }
 }
 
+for (let i = 0; i < 10; i++){
+    console.log("Repetition is a good thing.");
+}
