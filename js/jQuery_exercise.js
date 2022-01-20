93039cb8553c8789661d71bd7bd6eb20725ba6bf
Noah Bruno <noah.bruno122@gmail.com>
2022-01-20 15:01:58 -0600

    jQuery

--- js/jQuery_exercise.js
+++ js/jQuery_exercise.js
@@ -2,8 +2,37 @@
 //     alert( 'The page has finished loading!' );
 // }
 
-$(document).ready();
+// $(document).ready();
+//
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+// $("li").css("font-size", "20px");
+//
+// $("h1,p,li").css("background-color", "yellow")
+//
+// $(document).ready(function(){
+//     alert($("h1").text());
+// })
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+$("#head").click(
+    function(){
+        $(this).css("background", "orange");
+    }
+);
+
+$("p").dblclick(
+    function(){
+        $(this).css("font-size", "18px");
+    }
+)
+
+$("li").hover(
+    function(){
+        $(this).css("color", "red");
+    },
+    function(){
+        $(this).css("color", "black");
+    }
+)
