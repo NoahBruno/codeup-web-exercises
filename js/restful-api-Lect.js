86ac0a9b6dd3cf161565896a158a4e029ef3ff52
Noah Bruno <noah.bruno122@gmail.com>
2022-02-09 11:13:48 -0600

    restful

---
+++ js/restful-api-Lect.js
@@ -0,0 +1,31 @@
+// const url= 'https://codeup-restful-example.glitch.me/reviews'
+//
+// const reqbody= {
+//     "id":2,
+//     "restaurant":1,
+// }
+//
+// const options = {
+//     method: 'POST'
+//     headers: {
+//         'Content-Type' : 'application/json'
+//     }
+// }
+
+const reviewObj = {
+    restaurant_id: 1,
+    name: 'Codey',
+    rating: 5,
+    comments: "This is a really good place for coding and eating"
+};
+const url = 'https://codeup-restful-example.glitch.me/reviews';
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reviewObj),
+};
+fetch(url, options)
+    .then( response => console.log(response) ) /* review was created successfully */
+    .catch( error => console.error(error) ); /* handle errors */
